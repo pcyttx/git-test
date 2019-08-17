d919f8abaa1e7178616099433a865b814deda5e5
pcyttx <2398250143@qq.com>
2019-08-17 10:08:12 +0800

    提交到本地仓库test3

--- demo1_factoryStrategy/src/gitTest/Git.java
+++ demo1_factoryStrategy/src/gitTest/Git.java
@@ -3,6 +3,9 @@ package gitTest;
 public class Git {
     public static void main(String[] args) {
         System.out.println("gitTest success");
+        System.out.println("gitTest success");
+        System.out.println("gitTest success");
+        System.out.println("gitTest success");
     }
 
 }
